e1247aebdf5f39342f30bf24b703d9b4db3a73ef
mrgoutam <mrgoutam8.3@gmail.com>
2019-05-12 20:50:53 +0530

    simple implicit broadcast reciever completed

---
+++ app/src/main/java/in/goutamstark/examplebroadcastreceiver/ExampleBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package in.goutamstark.examplebroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.widget.Toast;
+
+public class ExampleBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())){
+            Toast.makeText(context, "Boot Completed", Toast.LENGTH_SHORT).show();
+        }
+
+        if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())){
+            Toast.makeText(context, "Connectivity Change", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
